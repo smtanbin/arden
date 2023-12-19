efb710353e3146551d0bc42c5f0b06e4adbb2b45
Tanbin <mds.tanbin@gmail.com>
2023-12-19 18:25:09 +0600

    ...

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   define: {
     "process.env": process.env,
   },
